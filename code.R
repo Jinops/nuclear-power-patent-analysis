4043a609e614b30746f5b240883506cc593c724c
jinops <jinwoo.develop@gmail.com>
2023-05-27 19:18:13 +0900

    Add group barplot (country-year,count)

--- code.R
+++ code.R
@@ -1,22 +1,34 @@
 data <- read.csv("sample.csv", fileEncoding='euc-kr')
 
-
-### Total Patent count by year
+### Total Patents  by year
 sprintf('count of row: %d', nrow(data))
 sprintf('count of column: %d', ncol(data))
 print(colnames(data))
 
-year <- data$Publication.Year
-year_counts <- table(year)
+yearData <- data$Publication.Year
+year_counts <- table(yearData)
 print(year_counts)
-barplot(year_counts, main="total patent count by year", xlab="year", ylab="patent count")
+barplot(year_counts, 
+        main="Total patents by year", xlab="year", ylab="patent count")
 
-###
+### TOP application authority (country)
 max <- 3
-country <- data$Current.Assignee.Country
-country_counts <- table(country)
-print(country_count)
-country_counts <- sort(country_count, decreasing=TRUE)
-country_count <- country_count[1:max]
+country_counts <- table(data$Current.Assignee.Country)
 print(country_counts)
-barplot(country_counts, main=sprintf("TOP%d application authority", max), xlab="year", ylab="country")
+country_counts_sorted <- sort(country_counts, decreasing=TRUE)
+country_counts_top <- country_counts_sorted[1:max]
+print(country_counts_top)
+barplot(country_counts_top, 
+        main=sprintf("TOP%d application authority", max), xlab="country", ylab="count")
+
+### TOP countries' patents by year
+top_country <- rownames(country_counts_top)
+print(country_counts_top)
+print(typeof(top_country))
+topCountryData <- data[data$Current.Assignee.Country %in% names(country_counts_top), ]
+topCountryData_counts <- table(topCountryData$Current.Assignee.Country, topCountryData$Publication.Year)
+print(topCountryData_counts)
+barplot(topCountryData_counts, beside = TRUE,
+        main=sprintf("TOP%d countries' patents by year", max), xlab = "Year", ylab = "Count",
+        legend=rownames(topCountryData_counts), col = c("red", "green", "blue"))
+
