79f93e31a880ebd4a9dcc2cefda02c74f25202ad
jinops <jinwoo.develop@gmail.com>
2023-05-29 22:18:49 +0900

    Add script-all code

---
+++ code_all.R
@@ -0,0 +1,15 @@
+file_names <- list(
+  'G21B',
+  'G21C',
+  'G21D',
+  'G21F',
+  'G21G',
+  'G21H',
+  'G21J',
+  'G21K'
+)
+file_names <- append(file_names, 'All')
+
+for(file_name in file_names){
+  source("code.R")
+}
