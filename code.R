1dd737f71eae673145774388a887b56e84140e85
jinops <jinwoo.develop@gmail.com>
2023-05-30 20:06:55 +0900

    Modify country data

--- code.R
+++ code.R
@@ -17,19 +17,26 @@ pdf(paste('plots/',file_name,'.pdf',sep=''), width= 5, height= 5)
 max <- 5 # TOP countries number
 
 # Preprocessing
-data$year <- as.numeric(substr(data$patent_date, 1, 4))
-# app year: sapply(1:nrow(data), function(i) as.numeric(substr(data[i,]$applications[[1]]$app_date,1, 4)))
-data$country <- sapply(1:nrow(data), function(i) data[i,]$inventors[[1]]$inventor_country[1])
+patent_year <- as.numeric(substr(data$patent_date, 1, 4))
+app_year <- sapply(1:nrow(data), function(i) as.numeric(substr(data[i,]$applications[[1]]$app_date,1, 4)))
+data$year <- patent_year
+for (i in 1:nrow(data)){
+  country <- data[i,]$assignees[[1]]$assignee_country[1]
+  if (is.na(country))
+    country <- data[i,]$inventors[[1]]$inventor_country[1]
+  data$country[i] <- country
+}
+print(data$country)
 
-#1 Patent registration by year
-title <- sprintf("Patent registration by year\n(%s)", file_name)
+#1 Patent registrations by year
+title <- sprintf("Patent registrations by year\n(%s)", file_name)
 year_table <- table(data$year)
 print(year_table)
 barplot(year_table, 
         main=title, xlab="year", ylab="patent count")
 
-#2 TOP patent inventors' counties
-title <- sprintf("TOP%d patent inventors' counties\n(%s)", max, file_name)
+#2 TOP counties
+title <- sprintf("TOP%d counties\n(%s)", max, file_name)
 country_table <- table(data$country)
 print(country_table)
 country_table_sorted <- sort(country_table, decreasing=TRUE)
@@ -38,8 +45,8 @@ print(country_table_top)
 barplot(country_table_top, 
         main=title, xlab="country", ylab="count")
 
-#3 TOP countries' patents by year
-title <- sprintf("TOP%d countries' patents by year\n(%s)", max, file_name)
+#3 TOP countries' by year
+title <- sprintf("TOP%d countries by year\n(%s)", max, file_name)
 data_top <- data[data$country %in% names(country_table_top), ]
 country_year_table_top <- table(data_top$country, data_top$year)
 print(country_year_table_top)
@@ -50,3 +57,4 @@ barplot(country_year_table_top, beside = TRUE,
         col=colors)
 
 dev.off()
+
